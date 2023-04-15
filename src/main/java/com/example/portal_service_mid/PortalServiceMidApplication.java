729c69a9a42663f2c2d0b18a8fb9781313df64ec
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-04-15 21:15:51 +0900

    settings

---
+++ src/main/java/com/example/portal_service_mid/PortalServiceMidApplication.java
@@ -0,0 +1,13 @@
+package com.example.portal_service_mid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalServiceMidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalServiceMidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/portal_service_mid/PortalServiceMidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portal_service_mid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalServiceMidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
