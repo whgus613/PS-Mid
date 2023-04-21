e5b657375eed3957c44cd591e0b752de2b0f60fa
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-04-21 16:23:02 +0900

    4번 - 재커밋

--- src/main/java/com/example/portal_service_mid/user/DaoFactory.java
+++ src/main/java/com/example/portal_service_mid/user/DaoFactory.java
@@ -1,19 +1,14 @@
 package com.example.portal_service_mid.user;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 public class DaoFactory {
 
-    @Bean
     public UserDao getUserDao(){
         UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
 
-    @Bean
-    public ConnectionMaker connectionMaker() {
+    private ConnectionMaker connectionMaker() {
         return new JejuConnectionMaker();
     }
 
