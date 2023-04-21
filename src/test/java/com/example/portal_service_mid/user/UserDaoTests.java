65710731340f7097901f254a9c9f649e58d692e9
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-04-21 16:25:33 +0900

    5번

--- src/test/java/com/example/portal_service_mid/user/UserDaoTests.java
+++ src/test/java/com/example/portal_service_mid/user/UserDaoTests.java
@@ -1,6 +1,9 @@
 package com.example.portal_service_mid.user;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,13 +13,20 @@ import static org.hamcrest.Matchers.in;
 import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
+
+    private static UserDao userDao;
+
+    @BeforeAll
+    public static void UserDaoTests(){
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactory.class);
+        userDao = applicationContext.getBean("userDao", UserDao.class);
+    }
+
     @Test
     public void get() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "hulk";
         String password = "1234";
-        ConnectionMaker connectionMaker = new JejuConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -30,8 +40,6 @@ public class UserDaoTests {
         User user = new User();
         user.setName(name);
         user.setPassword(password);
-        DaoFactory daoFactory = new DaoFactory();
-        UserDao userDao = daoFactory.getUserDao();
         userDao.insert(user);
         assertThat(user.getId(), greaterThan(1l));
 
