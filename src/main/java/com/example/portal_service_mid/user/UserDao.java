650b8e2e5c0a6b88d751ed881d415bf7013be07a
whgus613 <79959287+whgus613@users.noreply.github.com>
2023-04-21 16:50:08 +0900

    10번

--- src/main/java/com/example/portal_service_mid/user/UserDao.java
+++ src/main/java/com/example/portal_service_mid/user/UserDao.java
@@ -5,128 +5,40 @@ import java.sql.*;
 
 public class UserDao {
 
-    private DataSource dataSource;
+    private JdbcContext jdbcContext;
 
-    public UserDao(DataSource dataSource){
-        this.dataSource = dataSource;
+    public UserDao(JdbcContext jdbcContext){
+        this.jdbcContext = jdbcContext;
     }
 
     public User findById(Long id) throws ClassNotFoundException, SQLException {
 
         StatementStrategy statementStrategy = new FindStatementStrategy(id);
+        return jdbcContext.jdbcContextForFind(statementStrategy);
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-        User user = null;
-
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection);
-            resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()){
-                user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-            }
-        } finally {
-            try {
-                resultSet.close();
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            }
-            catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
-
-        return user;
 
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException{
 
         StatementStrategy statementStrategy = new InsertStatementStrategy(user);
+        jdbcContext.jdbcContextForInsert(user, statementStrategy);
 
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        ResultSet resultSet = null;
-
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-            resultSet = preparedStatement.getGeneratedKeys();
-            resultSet.next();
-            user.setId(resultSet.getLong(1));
-        } finally {
-            try {
-                resultSet.close();
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            }
-            catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
     }
 
     public void update(User user) throws SQLException{
 
         StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
-        jdbcContextForUpdate(statementStrategy);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
     }
 
     public void delete(Long id) throws SQLException{
 
         StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
-        jdbcContextForUpdate(statementStrategy);
+        jdbcContext.jdbcContextForUpdate(statementStrategy);
 
     }
 
-    private void jdbcContextForUpdate(StatementStrategy statementStrategy) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
 
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection);
-            preparedStatement.executeUpdate();
-        } finally {
-            try {
-                preparedStatement.close();
-            }
-            catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            }
-            catch (SQLException e){
-                e.printStackTrace();
-            }
-        }
-    }
 
 }
